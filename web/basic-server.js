584e2d33e5f96031fa8a6bf7f6707fa8e274f69b
SYU88 <sarah.m.yu88@gmail.com>
2015-06-03 16:04:19 -0700

    fs attempt

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,6 +1,12 @@
 var http = require("http");
 var handler = require("./request-handler");
 var initialize = require("./initialize.js");
+var fs = require("fs");
+var url = require("url");
+var path = require("path");
+var static = require("node-static");
+
+var file = new static.Server("/Users/HR10/Desktop/historian/web/public");
 
 // Why do you think we have this here?
 // HINT:It has to do with what's in .gitignore
@@ -8,6 +14,40 @@ initialize();
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+// var server = http.createServer(handler.handleRequest);
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += 'public/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(port, ip)
+
+console.log("Static file server running at\n  => http://localhost:" + port);
+// var server = http.createServer();
+
+// console.log("Listening on http://" + ip + ":" + port);
+// server.listen(port, ip);
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,22 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var request = require('request');
+var headers = require('./http-helpers.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+  if(req.url.match('/')) {
+    if(req.method === 'GET') {
+      res.writeHead(200, headers.headers);
+      res.end(archive.paths.list);
+      console.log(headers.headers);
+    }
+  }
   res.end(archive.paths.list);
 };
+
+exports.sendResponse = function (response, data, statusCode) {
+  var statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(data);
+}
